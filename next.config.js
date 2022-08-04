ea0cdb253f61a363497c2b77846ca327adabdd9d
jchaselubitz <jchaselubitz@gmail.com>
2022-08-04 17:51:26 +0200

    change nextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+module.exports = {
+  plugin: (schema, documents, config) => {
+    return ["typescript", "typescript-react-query", "typescript-react-apollo"];
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    //this is to fix an issue with webpack not finding the electron module
+    config.module.rules.push({
+      test: /\.md$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: "markdown-loader",
+        },
+      ],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
